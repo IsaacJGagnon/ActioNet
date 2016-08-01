bbe194e1a09ac1aec30c945fc787036656b97d82
IsaacJGagnon <isaacjgagnon@gmail.com>
2016-08-01 12:06:26 -0400

    Add files via upload

--- Q1/EllipseTest.java
+++ Q1/EllipseTest.java
@@ -17,43 +17,32 @@ public class EllipseTest{
 	
 	@Test
 	public void areaTestInts(){
-		assertEquals(a(5,5), r1.getArea(), 0.001);
-		assertEquals(a(7,4), r2.getArea(), 0.001);
-		assertEquals(a(4,7), r3.getArea(), 0.001);
+		assertEquals(78.54, r1.getArea(), 0.001);
+		assertEquals(87.96, r2.getArea(), 0.01);
+		assertEquals(87.96, r3.getArea(), 0.01);
+		assertEquals(r2.getArea(), r3.getArea(), 0.001);
 	}
 	
 	@Test
 	public void areaTestDouble(){
-		assertEquals(a(1.1,1.1), r4.getArea(), 0.001);
-		assertEquals(a(0.1,0.1), r5.getArea(), 0.001);
-		assertEquals(a(0.0,0.1), r6.getArea(), 0.001);
-		assertEquals(a(pi,pi), r7.getArea(), 0);
+		assertEquals(3.8, r4.getArea(), 0.1);
+		assertEquals(0.031, r5.getArea(), 0.001);
+		assertEquals(0, r6.getArea(), 0.001);
+		assertEquals(31.00627668, r7.getArea(), 0.00000001);
 	}
 	
 	@Test
 	public void perimiterTestInts(){
-		assertEquals(p(5,5), r1.getPerimeter(), 0.001);
-		assertEquals(p(7,4), r2.getPerimeter(), 0.001);
-		assertEquals(p(7,4), r3.getPerimeter(), 0.001);
+		assertEquals(31.42, r1.getPerimeter(), 0.01);
+		assertEquals(35.2, r2.getPerimeter(), 0.1);
+		assertEquals(35.2, r3.getPerimeter(), 0.1);
 	}
 	
 	@Test
 	public void perimiterTestDouble(){
-		assertEquals(p(1.1,1.1), r4.getPerimeter(), 0.001);
-		assertEquals(p(0.1,0.1), r5.getPerimeter(), 0.001);
-		assertEquals(p(0.1,0.0), r6.getPerimeter(), 0.001);
-		assertEquals(p(pi,pi), r7.getPerimeter(), 0);	
-	}
-	
-	public double a(double x, double y){
-		return pi * x * y;
-	}
-	
-	public double p(double x, double y){
-		if(x == 0 && y == 0){
-			return x;
-		}
-		double h = Math.pow(x-y,2)/Math.pow(x+y,2);
-		return pi * (x+y) *(1+((3*h)/(10+Math.sqrt(4-(3*h)))));
+		assertEquals(6.91, r4.getPerimeter(), 0.01);
+		assertEquals(0.63, r5.getPerimeter(), 0.01);
+		assertEquals(0.399839065, r6.getPerimeter(), 0.00000001);
+		assertEquals(19.7392088, r7.getPerimeter(), 0.000001);	
 	}
 }
