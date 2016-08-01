20f0f8730016910c5949054fe296396abe766981
IsaacJGagnon <isaacjgagnon@gmail.com>
2016-08-01 11:50:02 -0400

    Add files via upload

--- Q1/TriangleTest.java
+++ Q1/TriangleTest.java
@@ -7,8 +7,8 @@ public class TriangleTest{
 	
 	//ints
 	Shape r1 = new Triangle();
-	Shape r2 = new Triangle(7,4,3);
-	Shape r3 = new Triangle(3,7,4);
+	Shape r2 = new Triangle(7,4,8);
+	Shape r3 = new Triangle(8,7,4);
 	//doubles
 	Shape r4 = new Triangle(1.1,1.1,1.1);
 	Shape r5 = new Triangle(0.1,0.1,0.1);
@@ -17,40 +17,33 @@ public class TriangleTest{
 	
 	@Test
 	public void areaTestInts(){
-		assertEquals(a(3,4,5), r1.getArea(), 0.001);
-		assertEquals(a(7,4,3), r2.getArea(), 0.001);
-		assertEquals(a(3,7,4), r3.getArea(), 0.001);
+		assertEquals(6, r1.getArea(), 0.001);
+		assertEquals(14, r2.getArea(), 0.01);
+		assertEquals(14, r3.getArea(), 0.01);
+		assertEquals(r2.getArea(), r3.getArea(), 0.001);
 	}
 	
 	@Test
 	public void areaTestDouble(){
-		assertEquals(a(1.1,1.1,1.1), r4.getArea(), 0.001);
-		assertEquals(a(0.1,0.1,0.1), r5.getArea(), 0.001);
-		assertEquals(a(0.0,0.0,0.1), r6.getArea(), 0.001);
-		assertEquals(a(pi,pi,pi), r7.getArea(), 0);
+		assertEquals(0.52, r4.getArea(), 0.005);
+		assertEquals(0.00433, r5.getArea(), 0.00001);
+		assertEquals(0, r6.getArea(), 0.001);
+		assertEquals(4.273664067, r7.getArea(), 0.00000001);
 	}
 	
 	@Test
 	public void perimiterTestInts(){
-		assertEquals(p(3,4,5), r1.getPerimeter(), 0.001);
-		assertEquals(p(7,4,3), r2.getPerimeter(), 0.001);
-		assertEquals(p(3,7,4), r3.getPerimeter(), 0.001);
+		assertEquals(12, r1.getPerimeter(), 0.001);
+		assertEquals(19, r2.getPerimeter(), 0.001);
+		assertEquals(19, r3.getPerimeter(), 0.001);
+		assertEquals(r2.getPerimeter(), r3.getPerimeter(), 0.001);
 	}
 	
 	@Test
 	public void perimiterTestDouble(){
-		assertEquals(p(1.1,1.1,1.1), r4.getPerimeter(), 0.001);
-		assertEquals(p(0.1,0.1,0.1), r5.getPerimeter(), 0.001);
-		assertEquals(p(0.0,0.0,0.1), r6.getPerimeter(), 0.001);
-		assertEquals(p(pi,pi,pi), r7.getPerimeter(), 0);	
-	}
-	
-	public double a(double x, double y, double z){
-		double s = (x + y + z)/2;
-		return Math.sqrt(s * (s - x) * (s - y) * (s - z));
-	}
-	
-	public double p(double x, double y, double z){
-		return x + y + z;
+		assertEquals(3.3, r4.getPerimeter(), 0.001);
+		assertEquals(0.3, r5.getPerimeter(), 0.001);
+		assertEquals(0.1, r6.getPerimeter(), 0.001);
+		assertEquals(pi*3, r7.getPerimeter(), 0);	
 	}
 }
